a0e3b96656dc7e13902d5fca4e8bc3201175330d
Shawn Nock <nock@nocko.se>
2014-10-22 11:04:07 -0400

    Middle of refactor, broken

---
+++ src/avahi.c
@@ -0,0 +1,115 @@
+#include <avahi-common/simple-watch.h>
+#include <avahi-common/error.h>
+#include <avahi-common/malloc.h>
+#include <avahi-common/domain.h>
+#include <avahi-common/llist.h>
+#include <avahi-client/client.h>
+#include <avahi-client/lookup.h>
+
+static AvahiSimplePoll *simple_poll = NULL;
+static AvahiClient *client = NULL;
+
+static void host_name_resolver_callback(AvahiHostNameResolver *r,
+                                        AVAHI_GCC_UNUSED AvahiIfIndex interface,
+                                        AVAHI_GCC_UNUSED AvahiProtocol protocol,
+                                        AvahiResolverEvent event,
+                                        const char *name, const AvahiAddress *a,
+                                        AVAHI_GCC_UNUSED AvahiLookupResultFlags
+                                            flags,
+                                        AVAHI_GCC_UNUSED void *userdata) {
+
+  assert(r);
+
+  switch (event) {
+  case AVAHI_RESOLVER_FOUND: {
+    char address[AVAHI_ADDRESS_STR_MAX];
+
+    avahi_address_snprint(address, sizeof(address), a);
+
+    printf("%s\t%s\n", name, address);
+    g_config.post_url = malloc((size_t)(strlen(g_config.c_post_url_template)+strlen(address)));
+    sprintf(g_config.post_url, g_config.c_post_url_template, address);
+    g_config.configured = true;
+    break;
+  }
+
+  case AVAHI_RESOLVER_FAILURE:
+
+    fprintf(stderr, _("Failed to resolve host name: '%s': %s\n"), name,
+            avahi_strerror(avahi_client_errno(client)));
+
+    break;
+  }
+
+  avahi_host_name_resolver_free(r);
+  avahi_simple_poll_quit(simple_poll);
+}
+
+static void client_callback(AvahiClient *c, AvahiClientState state,
+                            AVAHI_GCC_UNUSED void *userdata) {
+  switch (state) {
+  case AVAHI_CLIENT_FAILURE:
+    fprintf(stderr, _("Client failure, exiting: %s\n"),
+            avahi_strerror(avahi_client_errno(c)));
+    avahi_simple_poll_quit(simple_poll);
+    break;
+
+  case AVAHI_CLIENT_S_REGISTERING:
+  case AVAHI_CLIENT_S_RUNNING:
+  case AVAHI_CLIENT_S_COLLISION:
+  case AVAHI_CLIENT_CONNECTING:
+    ;
+  }
+}
+
+void configure_via_avahi(config_t *cfg) {
+  int config_use_avahi;
+  AvahiClient *client = NULL;
+  AvahiServiceBrowser *sb = NULL;
+  const char *config_avahi_server;
+  int error;
+
+  if (config_lookup_bool(cfg, "use_avahi", &config_use_avahi) && config_use_avahi) {
+    if (config_lookup_string(cfg, "avahi_name", &config_avahi_server))
+      if (config_lookup_string(cfg, "post_url_template", &config_post_url_template)) {
+        printf(_("Using Avahi/Zeroconf: trying to resolve: %s\n"),
+               config_avahi_server);
+        if (!(simple_poll = avahi_simple_poll_new())) {
+          fprintf(stderr, _("Failed to create simple poll object.\n"));
+	  ret = ERR_AVAHI_FAIL;
+          goto cleanup;
+        }
+
+        while (!(client = avahi_client_new(avahi_simple_poll_get(simple_poll), 0,
+					   client_callback, NULL, &error)));
+        if (!(avahi_host_name_resolver_new(
+                client, AVAHI_IF_UNSPEC, AVAHI_PROTO_UNSPEC, config_avahi_server,
+                AVAHI_PROTO_UNSPEC, 0, host_name_resolver_callback, NULL))) {
+          fprintf(stderr, _("Failed to create host name resolver: %s\n"),
+                  avahi_strerror(avahi_client_errno(client)));
+	  ret = ERR_AVAHI_FAIL;
+          goto cleanup;
+        }
+        /* Configuration happens (or not) during this poll loop via
+           the host_name_resolver_callback function */
+        avahi_simple_poll_loop(simple_poll);
+      } else {
+        fprintf(stderr, _("Avahi configuration failed, no post_url_template in "
+                          "config file.\n"),
+                avahi_strerror(error));
+	ret = ERR_BAD_CONFIG;
+        goto cleanup;
+      }
+  }
+}
+
+void cleanup_avahi(void) {
+  if (sb)
+    avahi_service_browser_free(sb);
+
+  if (client)
+    avahi_client_free(client);
+
+  if (simple_poll)
+    avahi_simple_poll_free(simple_poll);
+}
---
+++ src/ble.c
@@ -0,0 +1,93 @@
+static int scan_loop(int dd, uint8_t filter_type) {
+  /* Initialize Curl */
+  m_curl_init();
+  
+  json_object *ble_adv;
+  ble_adv = json_object_new_object();
+
+  unsigned char buf[HCI_MAX_EVENT_SIZE], *ptr;
+  struct hci_filter nf, of;
+  socklen_t olen;
+  int len;
+
+  olen = sizeof(of);
+  if (getsockopt(dd, SOL_HCI, HCI_FILTER, &of, &olen) < 0) {
+    printf(_("Could not get socket options\n"));
+    return -1;
+  }
+
+  hci_filter_clear(&nf);
+  hci_filter_set_ptype(HCI_EVENT_PKT, &nf);
+  hci_filter_set_event(EVT_LE_META_EVENT, &nf);
+
+  if (setsockopt(dd, SOL_HCI, HCI_FILTER, &nf, sizeof(nf)) < 0) {
+    perror(_("Could not set socket options\n"));
+    ret = ERR_BLUEZ_SOCKET;
+    goto cleanup;
+  }
+
+  while (1) {
+    evt_le_meta_event *meta;
+    le_advertising_info *info;
+
+    char addr[18];
+
+    while ((len = read(dd, buf, sizeof(buf))) < 0) {
+      if (errno == EINTR && signal_received == SIGINT) {
+        len = 0;
+        goto done;
+      }
+
+      if (errno == EAGAIN || errno == EINTR)
+        continue;
+      goto done;
+    }
+    timestamp = time(NULL);
+    ptr = buf + (1 + HCI_EVENT_HDR_SIZE);
+    len -= (1 + HCI_EVENT_HDR_SIZE);
+
+    meta = (void *)ptr;
+
+    if (meta->subevent != 0x02)
+      goto done;
+
+    int num_reports, offset = 0;
+    num_reports = meta->data[0];
+
+    for (int i = 0; i < num_reports; i++) {
+      info = (le_advertising_info *)(meta->data + offset + 1);
+
+      uint8_t data[info->length + 2];
+      int rssi;
+      data[info->length] = 0;
+      memcpy(data, &info->data, info->length);
+
+      ba2str(&info->bdaddr, addr);
+      ble_adv = json_object_new_object();
+      json_object_object_add(ble_adv, _("mac"), json_object_new_string(addr));
+      json_object_object_add(ble_adv, _("listener"),
+                             json_object_new_string(hostname));
+      json_object_object_add(ble_adv, _("timestamp"),
+                             json_object_new_int(timestamp));
+      json_object_object_add(ble_adv, _("data"),
+                             json_object_new_string((const char *)data));
+      offset += info->length + 11;
+      rssi = *((int8_t *)(meta->data + offset - 1));
+      json_object_object_add(ble_adv, _("rssi"), json_object_new_int(rssi));
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS,
+                       json_object_to_json_string(ble_adv));
+      curl_easy_perform(curl);
+#ifdef DEBUG
+      printf("%s\n", json_object_to_json_string(ble_adv));
+#endif /* DEBUG */
+    }
+  }
+
+done:
+  setsockopt(dd, SOL_HCI, HCI_FILTER, &of, sizeof(of));
+
+  if (len < 0)
+    return -1;
+
+  return 0;
+}
