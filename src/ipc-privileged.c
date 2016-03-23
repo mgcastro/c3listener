ede2484a571ba2823cff23ff0d3f6d9b4fb2c0e7
Shawn Nock <nock@nocko.se>
2016-03-23 12:14:26 -0400

    Include half-baked IPC to sqaush compilation error w/o revert

---
+++ src/ipc-privileged.c
@@ -0,0 +1,49 @@
+/* ipc-privileged.c - Shawn Nock; Jaycon Systems, LLC
+ *
+ *   IPC callbacks and side-effect functions running as root in the
+ *   parent process
+ */
+
+#include <string.h>
+
+#include <event2/buffer.h>
+#include <event2/bufferevent.h>
+
+#include <json-c/json.h>
+
+#include "ipc.h"
+#include "ipc-privileged.h"
+#include "log.h"
+
+void ipc_parent_readcb(struct bufferevent *bev, void *ctx) {
+  log_notice("Parent notified of bufferevent\n");
+  struct evbuffer *in_buf = bufferevent_get_input(bev);
+  while (evbuffer_get_length(in_buf) > 0) {
+    ipc_cmd_t *c = ipc_cmd_fetch_alloc(bev);
+    char *output = ipc_cmd_str(c);
+    log_notice("Parent RX: %s", output);
+    free(output);
+    ipc_resp_t *r = ipc_resp_alloc();
+    r->serial = c->serial;
+    r->success = true;
+    r->resp_l = 5;
+    r->resp = calloc(1, strlen("{}")+1);
+    strcpy(r->resp, "{}");
+    ipc_resp_send(bev, r);
+    ipc_resp_free(r);
+    ipc_cmd_free(c);
+  }
+}
+
+/* static void ipc_cmd_get_uci(struct evhttp_request *req, void *arg) { */
+/*   json_object *jobj = json_object_new_object(); */
+
+/*   json_object_object_add(jobj, "wired", */
+/* 			 uci_section_jobj("network.lan2")); */
+/*   json_object_object_add(jobj, "wireless", */
+/* 			 uci_section_jobj("wireless.@wifi-iface[0]")); */
+  
+/*   struct evbuffer *buf = evhttp_request_get_output_buffer(req); */
+/*   const char *json = json_object_to_json_string(jobj); */
+/*   return  */
+/* } */
---
+++ src/ipc-privileged.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void ipc_parent_readcb(struct bufferevent *, void *);
